65f1bdc4b31a6e1a3b00ffa89eed9989b8a1bb30
Moneyless <spped124589@naver.com>
2019-08-21 16:16:13 +0900

    Add files via upload

---
+++ Monitor.java
@@ -0,0 +1,56 @@
+
+public class Monitor {
+	private String mName, mProd, mPanel, mDate;
+	private int mInch, mWidth, mHeight, mPrice;
+	
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public String getmProd() {
+		return mProd;
+	}
+	public void setmProd(String mProd) {
+		this.mProd = mProd;
+	}
+	public String getmPanel() {
+		return mPanel;
+	}
+	public void setmPanel(String mPanel) {
+		this.mPanel = mPanel;
+	}
+	public String getmDate() {
+		return mDate;
+	}
+	public void setmDate(String mDate) {
+		this.mDate = mDate;
+	}
+	public int getmInch() {
+		return mInch;
+	}
+	public void setmInch(int mInch) {
+		this.mInch = mInch;
+	}
+	public int getmWidth() {
+		return mWidth;
+	}
+	public void setmWidth(int mWidth) {
+		this.mWidth = mWidth;
+	}
+	public int getmHeight() {
+		return mHeight;
+	}
+	public void setmHeight(int mHeight) {
+		this.mHeight = mHeight;
+	}
+	public int getmPrice() {
+		return mPrice;
+	}
+	public void setmPrice(int mPrice) {
+		this.mPrice = mPrice;
+	}
+	
+	
+}
